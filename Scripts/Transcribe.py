c59fedd3652be57b8dc8efb4ae7cc40921029612
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-25 22:22:03 -0300

    Fix audio and output directory paths

--- Scripts/Transcribe.py
+++ Scripts/Transcribe.py
@@ -1,4 +1,4 @@
 # Directory for Audio
-AUDIO = "C:\\Virtual Assistance\\AGATHA\\Sounds\\Speak.mp3"
+AUDIO = "C:\\VirtualAssistance\\AGATHA\\Sounds\\Speak.mp3"
 # Directory for OUTPUT
-OUTPUT = "C:\Virtual Assistance\AGATHA\Sounds\Speak.txt"
+OUTPUT = "C:\VirtualAssistance\AGATHA\Sounds\Speak.txt"
