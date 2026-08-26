bab8ed5c92715ab47f39ecc614fb00dce1336219
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-25 22:21:53 -0300

    Transcribe.py? Where are you?

--- Scripts/Speak.py
+++ Scripts/Speak.py
@@ -1,11 +1,12 @@
 import pyttsx3
 def AGATHAspeaks():
-    a = input('Speak it: ')
+    a = "Bottle of water, better, beer, work, lift"#"Remember, this is not just a story, this is our future"#input('Speak it: ')
     engine = pyttsx3.init()
     voices = engine.getProperty('voices') # Getting the property 'voices'
     engine.setProperty('voice', voices[1].id) # Define the voice. In this case is '1 Microsoft Zira Desktop - English (United States)' 
     #engine.say(a)
-    engine.say(a)
-    #engine.save_to_file(a, 'C:\\Virtual Assistance\\AGATHA\\Sounds\\Speak.mp3') # Saving the speak
+    #engine.say(a)
+    engine.save_to_file(a, 'C:\\Virtual Assistance\\AGATHA\\Sounds\\Speak.mp3') # Saving the speak
     engine.runAndWait()
     # file doesn't encontrado
+AGATHAspeaks()
---
+++ Scripts/Transcribe.py
@@ -0,0 +1,4 @@
+# Directory for Audio
+AUDIO = "C:\\Virtual Assistance\\AGATHA\\Sounds\\Speak.mp3"
+# Directory for OUTPUT
+OUTPUT = "C:\Virtual Assistance\AGATHA\Sounds\Speak.txt"
