98c347f465a7ebb2c005ac5fbe4bd676bb9ba4e6
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-25 22:22:24 -0300

    Fix file path for saving audio in Speak.py

--- Scripts/Speak.py
+++ Scripts/Speak.py
@@ -6,7 +6,7 @@ def AGATHAspeaks():
     engine.setProperty('voice', voices[1].id) # Define the voice. In this case is '1 Microsoft Zira Desktop - English (United States)' 
     #engine.say(a)
     #engine.say(a)
-    engine.save_to_file(a, 'C:\\Virtual Assistance\\AGATHA\\Sounds\\Speak.mp3') # Saving the speak
+    engine.save_to_file(a, 'C:\\VirtualAssistance\\AGATHA\\Sounds\\Speak.mp3') # Saving the speak
     engine.runAndWait()
     # file doesn't encontrado
 AGATHAspeaks()
