1b9ebf6285cde59ed9cff0629e6edc16b960f80e
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-26 21:41:12 -0300

    Micrphone works

--- Scripts/Microphone.py
+++ Scripts/Microphone.py
@@ -1,5 +1,17 @@
 import sounddevice as sd # Importing the module
-import Numpy # Import Numpy Library
+import numpy # Import Numpy Library
 
-myarray =
+ 
 fs = 44100
+duration = 4
+frames = fs*duration
+
+myarray = sd.rec( # It acess my input device and returns the array.
+    frames, 
+    samplerate=fs, 
+    channels=1
+)
+sd.wait() # It does Python to wait
+
+sd.play(myarray) # Playing my audio
+sd.wait()
